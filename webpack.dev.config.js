735f4e85c7a3b59abcd58a23b8fbaa16ae23bcca
xiaotuni <xiaotuni@gmail.com>
2017-09-06 23:15:14 +0800

    初始化项目

---
+++ webpack.dev.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: ['babel-loader?cacheDirectory=true'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+}
