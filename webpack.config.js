e23e02275233badcb0bc9841af5729d5cbbb2d4e
xiaotuni <xiaotuni@gmail.com>
2017-09-10 14:24:39 +0800

    生产环境配置

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: {
+    app: [
+      path.join(__dirname, 'src/index.js')
+    ],
+    vendor: ['react', 'react-router-dom', 'redux', 'react-dom', 'react-redux']
+  },
+  output: {
+    path: path.join(__dirname, './dist'),
+    // filename: 'bundle.js',
+    // chunkFilename: '[name].js'
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader?cacheDirectory=true'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 8192 }
+          }
+        ]
+      },
+    ]
+  },
+  resolve: {
+    alias: {
+      pages: path.join(__dirname, 'src/pages'),
+      component: path.join(__dirname, 'src/component'),
+      router: path.join(__dirname, 'src/router'),
+      actions: path.join(__dirname, 'src/redux/actions'),
+      reducers: path.join(__dirname, 'src/redux/reducers'),
+    }
+  },
+  devServer: {
+    port: 11111,
+    contentBase: path.join(__dirname, './dist'),
+    historyApiFallback: true,
+    host: '0.0.0.0'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(__dirname, 'src/index.html')
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+    }),
+  ],
+}
