5aa411198eeecace506a05cba301f82daad62004
xiaotuni <xiaotuni@gmail.com>
2017-09-11 00:00:27 +0800

    提取webpack公共的代码。

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,53 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const merge = require('webpack-merge');
+const CommCfg = require('./wepback.common.config.js');
+console.log('----------------dev.config---------');
+console.log(CommCfg);
+console.log('----------------dev.config---------');
 
-module.exports = {
+const devConfig = {
   devtool: 'inline-source-map',
   entry: {
     app: [
       'react-hot-loader/patch',
       path.join(__dirname, 'src/index.js')
-    ],
-    vendor: ['react', 'react-router-dom', 'redux', 'react-dom', 'react-redux']
+    ]
   },
   output: {
-    path: path.join(__dirname, './dist'),
-    // filename: 'bundle.js',
-    // chunkFilename: '[name].js'
     filename: '[name].[hash].js',
-    chunkFilename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        use: ['babel-loader?cacheDirectory=true'],
-        include: path.join(__dirname, 'src')
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: { limit: 8192 }
-          }
-        ]
-      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ]
-  },
-  resolve: {
-    alias: {
-      pages: path.join(__dirname, 'src/pages'),
-      component: path.join(__dirname, 'src/component'),
-      router: path.join(__dirname, 'src/router'),
-      actions: path.join(__dirname, 'src/redux/actions'),
-      reducers: path.join(__dirname, 'src/redux/reducers'),
-    }
   },
   devServer: {
     port: 11111,
@@ -55,13 +29,82 @@ module.exports = {
     historyApiFallback: true,
     host: '0.0.0.0'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.join(__dirname, 'src/index.html')
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-    }),
-  ],
 }
+const m = merge({
+  customizeArray(a, b, key) {
+    /*entry.app不合并，全替换*/
+    if (key === 'entry.app') {
+      return b;
+    }
+    return undefined;
+  }
+})(CommCfg, devConfig);
+
+console.log(m);
+console.log('-------merge---------');
+module.exports = m;
+
+
+// module.exports = {
+//   devtool: 'inline-source-map',
+//   entry: {
+//     app: [
+//       'react-hot-loader/patch',
+//       path.join(__dirname, 'src/index.js')
+//     ],
+//     vendor: ['react', 'react-router-dom', 'redux', 'react-dom', 'react-redux']
+//   },
+//   output: {
+//     path: path.join(__dirname, './dist'),
+//     // filename: 'bundle.js',
+//     // chunkFilename: '[name].js'
+//     filename: '[name].[hash].js',
+//     chunkFilename: '[name].[chunkhash].js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         use: ['babel-loader?cacheDirectory=true'],
+//         include: path.join(__dirname, 'src')
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader']
+//       },
+//       {
+//         test: /\.(png|jpg|gif)$/,
+//         use: [
+//           {
+//             loader: 'url-loader',
+//             options: { limit: 8192 }
+//           }
+//         ]
+//       },
+//     ]
+//   },
+//   resolve: {
+//     alias: {
+//       pages: path.join(__dirname, 'src/pages'),
+//       component: path.join(__dirname, 'src/component'),
+//       router: path.join(__dirname, 'src/router'),
+//       actions: path.join(__dirname, 'src/redux/actions'),
+//       reducers: path.join(__dirname, 'src/redux/reducers'),
+//     }
+//   },
+//   devServer: {
+//     port: 11111,
+//     contentBase: path.join(__dirname, './dist'),
+//     historyApiFallback: true,
+//     host: '0.0.0.0'
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       filename: 'index.html',
+//       template: path.join(__dirname, 'src/index.html')
+//     }),
+//     new webpack.optimize.CommonsChunkPlugin({
+//       name: 'vendor',
+//     }),
+//   ],
+// }
