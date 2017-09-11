e6f3e109e51d5c56f4a7701ade56665c8f9dd463
xiaotuni <xiaotuni@gmail.com>
2017-09-11 23:00:14 +0800

    aa

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
 import store, { BuildStore } from './redux/store';
 import ApiClient from './helpers/ApiClient'
-// console.log('---------store------------');
-// console.log(store);
 const ApiClientStore = BuildStore(new ApiClient());
-// console.log(ApiClientStore);
-// console.log('---------store------------');
 import getRouter from './router/router';
 
 /*初始化*/
