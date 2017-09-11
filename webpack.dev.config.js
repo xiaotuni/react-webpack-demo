a15b80d0cf3fbcadf74cc96ff10239c137e1603e
廖海兵 <liaohaibing@m24.cn>
2017-09-11 17:27:52 +0800

    添加postcss-loader,浏览器兼容问题

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CommCfg = require('./wepback.common.config.js');
 
 const devConfig = {
@@ -21,21 +23,27 @@ const devConfig = {
       {
         test: /\.scss$/,
         use: [
-          {
-            loader: "style-loader"
-          },
+          { loader: "style-loader" },
           {
             loader: "css-loader", options: {
-              sourceMap: true,
-              modules: true, minimize: true,
+              sourceMap: true, modules: true,
               localIdentName: '[local]_[hash:base64:5]'
             }
           },
           {
-            loader: "sass-loader", options: {
-              sourceMap: true
+            loader: 'postcss-loader', 
+            options: {
+              sourceMap: true,
+              config: {
+                path: 'postcss.config.js'
+              }
             }
-          }]
+          }
+          ,
+          {
+            loader: 'sass-loader', options: { sourceMap: true }
+          }
+        ]
       }
     ]
   },
@@ -45,6 +53,15 @@ const devConfig = {
     historyApiFallback: true,
     host: '0.0.0.0'
   },
+  // plugins: [
+  //   new webpack.LoaderOptionsPlugin({
+  //     options: {
+  //       postcss: [
+  //         autoprefixer(),
+  //       ]
+  //     }
+  //   })
+  // ]
 }
 const mergeCfg = merge({
   customizeArray(a, b, key) {
@@ -57,68 +74,3 @@ const mergeCfg = merge({
 })(CommCfg, devConfig);
 
 module.exports = mergeCfg;
-
-
-// module.exports = {
-//   devtool: 'inline-source-map',
-//   entry: {
-//     app: [
-//       'react-hot-loader/patch',
-//       path.join(__dirname, 'src/index.js')
-//     ],
-//     vendor: ['react', 'react-router-dom', 'redux', 'react-dom', 'react-redux']
-//   },
-//   output: {
-//     path: path.join(__dirname, './dist'),
-//     // filename: 'bundle.js',
-//     // chunkFilename: '[name].js'
-//     filename: '[name].[hash].js',
-//     chunkFilename: '[name].[chunkhash].js'
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         use: ['babel-loader?cacheDirectory=true'],
-//         include: path.join(__dirname, 'src')
-//       },
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader']
-//       },
-//       {
-//         test: /\.(png|jpg|gif)$/,
-//         use: [
-//           {
-//             loader: 'url-loader',
-//             options: { limit: 8192 }
-//           }
-//         ]
-//       },
-//     ]
-//   },
-//   resolve: {
-//     alias: {
-//       pages: path.join(__dirname, 'src/pages'),
-//       component: path.join(__dirname, 'src/component'),
-//       router: path.join(__dirname, 'src/router'),
-//       actions: path.join(__dirname, 'src/redux/actions'),
-//       reducers: path.join(__dirname, 'src/redux/reducers'),
-//     }
-//   },
-//   devServer: {
-//     port: 11111,
-//     contentBase: path.join(__dirname, './dist'),
-//     historyApiFallback: true,
-//     host: '0.0.0.0'
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       filename: 'index.html',
-//       template: path.join(__dirname, 'src/index.html')
-//     }),
-//     new webpack.optimize.CommonsChunkPlugin({
-//       name: 'vendor',
-//     }),
-//   ],
-// }
