1e48d5e6585fd9d6900c4768dacadb4de526343a
xiaotuni <xiaotuni@gmail.com>
2017-11-02 14:41:17 +0800

    运行方式

--- src/config.js
+++ src/config.js
@@ -1,6 +1,17 @@
-module.exports = {
+const env = {
+  development: {
+    serverApi: 'https://127.0.0.1:30081/webapi',
+    isProduction: false
+  },
+  production: {
+    serverApi: 'https://127.0.0.1:30081/webapi',
+    isProduction: true
+  }
+}[process.env.NODE_ENV || 'development'];
+
+module.exports = Object.assign({
   app: {
     BaseName: '/react/',
     BuildPath: '/dist/react'
   }
-};
+}, env);
